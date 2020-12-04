255d175176b276cab8479719f3d87728e733e2b9
RAVI <tryout1215@gmail.com>
2020-12-04 06:41:16 +0000

    dec4c

--- src/test/java/TestAzure/TestAzure/SelClasss.java
+++ src/test/java/TestAzure/TestAzure/SelClasss.java
@@ -2,15 +2,29 @@ package TestAzure.TestAzure;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SelClasss {
-	
-@Test
+	WebDriver d = null;
+@Test(priority=1)
 public void test() {
 	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\chromedriver.exe");
-	WebDriver d = new ChromeDriver();
+	 d = new ChromeDriver();
 			d.get("https://www.google.co.in");;
+	
+}
+@Test(priority=2)
+public void test2() {
+	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\chromedriver.exe");
+	Assert.assertTrue(d.getTitle().equalsIgnoreCase("Google"));
+	
+}
+
+@Test(priority=3)
+public void test3() {
+	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\chromedriver.exe");
+	Assert.assertTrue(d.getTitle().equalsIgnoreCase("Google1111111111111"));
 	
 }
 }
